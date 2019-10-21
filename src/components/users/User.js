4dc3d1bd7c51b5d009beac6adc490b82b842d5d3
adudley78 <dudleyadam@gmail.com>
2019-10-21 16:34:47 -0400

    Refactor user state to context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,15 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getRepos, repos, match }) => {
+const User = ({ getRepos, repos, match }) => {
+  const githubContext = useContext(GithubContext);
+
+  const { getUser, loading, user } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getRepos(match.params.login);
@@ -81,9 +86,7 @@ const User = ({ user, loading, getUser, getRepos, repos, match }) => {
 
 User.propTypes = {
   loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
   repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
   getRepos: PropTypes.func.isRequired
 };
 
